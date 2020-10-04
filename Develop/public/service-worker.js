f60878a23b8c47dbf7c5d0459b4f2c09789f2b85
jack <biffisbuffm@gmail.com>
2020-10-04 15:55:17 -0500

    file path

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -5,12 +5,20 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-     //"../models/transaction.js",
-     "./js/idb.js",
-    "./js/index.js",
-   // "../Develop/routes/api.js"
+    "/",
+     "/db.js"
+    // "/index.js",
+    // "/styles.css",
+    // "/icon-72x72.png",
+    // "/icon-96x96.png",
+    // "/icon-128x128.png",
+    // "/icon-144x144.png",
+    // "/icon-152x152.png",
+    // "/icon-192x192.png",
+    // "/icon-384x384.png",
+    // "/icon-512x512.png"
+
+
 ]
 
 
