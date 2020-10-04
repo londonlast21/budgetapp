2b746d010af02c9e35e22fd6987ed52d89b0bc17
jack <biffisbuffm@gmail.com>
2020-10-04 16:00:37 -0500

    file paths fixed

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -4,19 +4,19 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 
 
-const FILES_TO_CACHE = [
+var FILES_TO_CACHE = [
     "/",
-     "/db.js"
-    // "/index.js",
-    // "/styles.css",
-    // "/icon-72x72.png",
-    // "/icon-96x96.png",
-    // "/icon-128x128.png",
-    // "/icon-144x144.png",
-    // "/icon-152x152.png",
-    // "/icon-192x192.png",
-    // "/icon-384x384.png",
-    // "/icon-512x512.png"
+    "/js/idb.js",
+   "/js/index.js",
+    "/css/styles.css",
+     "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png"
 
 
 ]
